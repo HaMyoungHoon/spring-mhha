b82081deebc1e2eed0457bcbb36c453104cd63b0
하명훈 <basicouts@gmail.com>
2024-06-12 12:15:21 +0900

    async enable

--- src/main/kotlin/mhha/springmhha/SpringMhhaApplication.kt
+++ src/main/kotlin/mhha/springmhha/SpringMhhaApplication.kt
@@ -3,10 +3,11 @@ package mhha.springmhha
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.ApplicationPidFileWriter
-import org.springframework.boot.runApplication
 import org.springframework.context.ConfigurableApplicationContext
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
+@EnableAsync
 class SpringMhhaApplication {
     companion object {
         var ctx: ConfigurableApplicationContext? = null
