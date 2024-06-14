96593084023e19cdbcd27a3a89eeef93f32a918e
하명훈 <basicouts@gmail.com>
2024-06-14 15:19:47 +0900

    레퍼러 필터 수정

--- src/main/kotlin/mhha/springmhha/config/RefererCheckFilter.kt
+++ src/main/kotlin/mhha/springmhha/config/RefererCheckFilter.kt
@@ -2,8 +2,6 @@ package mhha.springmhha.config
 
 import jakarta.servlet.FilterChain
 import jakarta.servlet.ServletException
-import jakarta.servlet.ServletRequest
-import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpFilter
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
