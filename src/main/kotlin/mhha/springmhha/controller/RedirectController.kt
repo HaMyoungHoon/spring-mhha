e799e8da4d91393b80131aeec2e8625082e6a4e8
하명훈 <basicouts@gmail.com>
2024-06-14 15:19:00 +0900

    리다이렉트 컨트롤 오리진 추가

--- src/main/kotlin/mhha/springmhha/controller/RedirectController.kt
+++ src/main/kotlin/mhha/springmhha/controller/RedirectController.kt
@@ -1,10 +1,12 @@
 package mhha.springmhha.controller
 
 import io.swagger.v3.oas.annotations.tags.Tag
+import mhha.springmhha.config.FConstants
 import mhha.springmhha.model.common.IRestResult
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -13,6 +15,7 @@ import java.net.URI
 @Tag(name = "RedirectController")
 @RestController
 @RequestMapping(value = ["/"])
+@CrossOrigin(origins = [FConstants.HTTP_MHHA, FConstants.HTTPS_MHHA], allowedHeaders = ["*"])
 class RedirectController {
 	@GetMapping
 	fun redirectEmptyPath(): ResponseEntity<*> {
