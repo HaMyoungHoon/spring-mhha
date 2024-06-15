061879f9934c7298e340637da6390e9fd00f9823
하명훈 <basicouts@gmail.com>
2024-06-15 17:23:31 +0900

    SwaggerConfiguration 수정

--- src/main/kotlin/mhha/springmhha/config/SwaggerConfiguration.kt
+++ src/main/kotlin/mhha/springmhha/config/SwaggerConfiguration.kt
@@ -6,12 +6,15 @@ import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.info.License
 import io.swagger.v3.oas.models.servers.Server
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @OpenAPIDefinition(servers = [io.swagger.v3.oas.annotations.servers.Server(url = "/", description = "defaultPath")])
 @Configuration
 class SwaggerConfiguration {
+  @Value(value = "\${str.version}") lateinit var strVersion: String
+  @Value(value = "\${str.profile}") lateinit var strprofile: String
   @Bean fun openApi(): OpenAPI = OpenAPI()
     .info(v1Info())
     .servers(mutableListOf<Server?>().apply {
@@ -19,9 +22,9 @@ class SwaggerConfiguration {
       add(springHttpServer())
     })
   private fun v1Info() = Info()
-    .title("spring mhha")
+    .title("$strprofile spring mhha")
     .description("테스트 하려고 만든 거")
-    .version("1")
+    .version(strVersion)
     .license(License())
     .contact(Contact())
   private fun springHttpsServer() = Server().apply {
